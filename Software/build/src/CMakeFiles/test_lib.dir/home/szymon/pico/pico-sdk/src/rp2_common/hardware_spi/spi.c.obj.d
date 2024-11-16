c5100538d704274eeae4fcf66a1ca1c79e4fedbe
Szymon Wąchała <wachim17@gmail.com>
2024-11-16 15:12:16 +0100

    first commit

---
+++ Software/build/src/CMakeFiles/test_lib.dir/home/szymon/pico/pico-sdk/src/rp2_common/hardware_spi/spi.c.obj.d
@@ -0,0 +1,36 @@
+src/CMakeFiles/test_lib.dir/home/szymon/pico/pico-sdk/src/rp2_common/hardware_spi/spi.c.obj: \
+ /home/szymon/pico/pico-sdk/src/rp2_common/hardware_spi/spi.c \
+ /home/szymon/pico/pico-sdk/src/rp2_common/hardware_resets/include/hardware/resets.h \
+ /home/szymon/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/szymon/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/szymon/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /mnt/c/Users/szymo/Projects/pico-SPI-test-slave/build/generated/pico_base/pico/version.h \
+ /home/szymon/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /mnt/c/Users/szymo/Projects/pico-SPI-test-slave/build/generated/pico_base/pico/config_autogen.h \
+ /home/szymon/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/szymon/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/szymon/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/szymon/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/szymon/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/resets.h \
+ /home/szymon/pico/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/resets.h \
+ /home/szymon/pico/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /home/szymon/pico/pico-sdk/src/rp2_common/hardware_spi/include/hardware/spi.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/spi.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/spi.h \
+ /home/szymon/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h
